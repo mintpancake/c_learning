da2290c148286452934a94a2620da35dd6b851db
Dennis <bouncy.lu@hotmail.com>
2019-12-30 18:47:46 +0800

    clear test file

--- test/test.c
+++ test/test.c
@@ -1,26 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(){
-    int previousDegree = 0;
-    int changedDegree = 0;
-    for(; ; ){
-        printf("The cuurent angle is %d. \nEnter the angle you want to change: ", previousDegree);
-        if(scanf("%d", &changedDegree) != 1) break;
-        previousDegree = deg(changedDegree, previousDegree);
-    }
-    printf("The final angle is %d. \nCompleted. ", previousDegree);
+    
     return 0;
-}
-
-int deg(int changedDegree, int previousDegree){
-    int pointingDgree = 0;
-    pointingDgree = (previousDegree + changedDegree) % 360;
-    if(pointingDgree < 0){
-        pointingDgree += 360;
-    }
-    if(pointingDgree > 180){
-        pointingDgree -= 360;
-    }
-    return pointingDgree;
 }
